72409b8d61eaacf73da6ba21c3fd83054e2672e7
jakeiridion <emil.ej.jeleskovic@gmail.com>
2020-04-06 20:19:22 +0200

    Added a line in the SETTINGS.ini file. Better Error handling in the ConfigReader.py file.

--- dependencies/ConfigReader.py
+++ dependencies/ConfigReader.py
@@ -22,7 +22,19 @@ class Config:
             if os.path.isfile(self.client_secret_path) is False:
                 raise Exception("No such file or directory: " + self.client_secret_path)
 
+            # raises an exception if the time between the checks is negative or zero
+            if self.wait_between_check <= 0 or self.wait_between_error <= 0:
+                raise Exception("A Negative int or zero was set in the SETTINGS.ini file under the wait_between_... "
+                                "section.")
+
+            if self.wait_between_check > 31622400 or self.wait_between_error > 31622400:
+                raise Exception("The waiting time is over a year. It needs to be under, or 31622400 seconds.")
+
         except Exception as exception:
+            print()
+            print("An Error Occurred with your Settings:")
+            print(exception)
+            print()
             write_log("An Error Occurred with your Settings", exception)
             exit()
 
